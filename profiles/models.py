5c0b2e31bab52b2132c0b6c779c0fffe272ca398
nikitalutsai <pythonplant12@gmail.com>
2023-02-07 21:39:03 +0100

    Work with forms and models to upload the file

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    # upload_to will dive into a root directory to find a file, if not defined 
+    # in settings.py (MEDIA_ROOT = BASE_DIR / "uploads”)
+    image = models.FileField(upload_to="images")
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
+from .forms import ProfileForm
+from .models import UserProfile
 # Create your views here.
 
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html", {
+            "form": form
+        })
 
     def post(self, request):
-        pass
+        submitted_file = ProfileForm(request.POST, request.FILES)
+        if submitted_file.is_valid():
+            # Now name of request.FILES will be the name of a method in FormClass
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles")
+        else:
+            return render(request, "profiles/create_profile.html", {
+                "form": submitted_file
+            })
